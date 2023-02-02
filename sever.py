c0e9b85a1cc9433f2faed570ef67e3a75765043e
yuorei <yuorei71@gmail.com>
2023-02-02 16:17:51 +0900

    add 関数の呼び出し

--- sever.py
+++ sever.py
@@ -1,16 +1,27 @@
 import json
+import use_model
 from flask import *
 
 app = Flask(__name__)
 
 @app.route("/post", methods=["POST"])
 def post_souse_code():
-    code = request.json['sourceCode']
+    problem_id=request.json['problemId']
+    language=request.json["language"]
+    source_code = request.json['sourceCode']
 
     # ここで機械学習用のコードを関数に入れて結果を受け取る
+    use_model.probability_error_source_code(source_code)
 
     result = {
-      "sourceCode": code
+      "problemId": problem_id,
+      "language" : language,
+      "result": [{ #TODO 現在サンプルを入れているため結果を入れるようにする
+          "lineNumber": 1,
+          "estimatedCorrectSentence": "#include<stdio.h>",
+          "probability": 0.98,
+        }
+      ]
     }
     return jsonify({"result": result})
 
